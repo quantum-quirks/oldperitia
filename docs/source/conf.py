3d31da3d46bfdfe2664e2972c13f5e25b7160d4a
Gerrishon Sirere <s.gerrishon@gmail.com>
2023-01-23 16:15:23 +0300

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Peritia'
+copyright = '2023, Scalabli'
+author = 'Gerrishon'
 
 release = '0.1'
 version = '0.1.0'
